163d6b3fd944f777e58a554f5ab056299ef01358
Victor Yves Crispim <rotcivyves@hotmail.com>
2020-04-25 17:36:07 -0300

    feat: create PokemonType enum

---
+++ pokedex/PokemonType.swift
@@ -0,0 +1,28 @@
+//
+//  PokemonType.swift
+//  pokedex
+//
+//  Created by Victor Yves Crispim on 25/04/20.
+//  Copyright © 2020 yves. All rights reserved.
+//
+
+enum PokemonType: String {
+    case bug
+    case dark
+    case dragon
+    case electric
+    case fairy
+    case fighting
+    case fire
+    case flying
+    case ghost
+    case grass
+    case ground
+    case ice
+    case normal
+    case poison
+    case psychic
+    case rock
+    case steel
+    case water
+}
