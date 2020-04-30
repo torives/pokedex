f5696f6c9e491b40924543734c9d778e9fc0f0c4
Victor Yves Crispim <rotcivyves@hotmail.com>
2020-04-29 22:36:35 -0300

    feat: create StatefulPreviewWrapper
    To allow use of @State inside a preview

---
+++ pokedex/Components/StatefulPreviewWrapper.swift
@@ -0,0 +1,25 @@
+//
+//  StatefulPreviewWrapper.swift
+//  pokedex
+//
+//  Created by Victor Yves Crispim on 29/04/20.
+//  Copyright © 2020 yves. All rights reserved.
+//
+
+import SwiftUI
+
+#if DEBUG
+struct StatefulPreviewWrapper<Value, Content: View>: View {
+    @State var value: Value
+    var content: (Binding<Value>) -> Content
+    
+    var body: some View {
+        content($value)
+    }
+    
+    init(_ value: Value, content: @escaping (Binding<Value>) -> Content) {
+        self._value = State(wrappedValue: value)
+        self.content = content
+    }
+}
+#endif
