ea4558b7c71d0005ff8ebd7c0904f5f065115ca4
Victor Yves Crispim <rotcivyves@hotmail.com>
2020-05-03 16:12:54 -0300

    Revert "brincando com alinhamento"
    This reverts commit b110c4f83ca8ad2df1633c638770954e7ffc4d12.

--- pokedex/Components/RangeSlider.swift
+++ pokedex/Components/RangeSlider.swift
@@ -8,35 +8,7 @@
 
 import SwiftUI
 
-
-//swiftlint:disable identifier_name
-extension VerticalAlignment {
-    private enum MyVerticalAlignment: AlignmentID {
-        static func defaultValue(in d: ViewDimensions) -> CGFloat {
-            return d[VerticalAlignment.center]
-        }
-    }
-    
-    static let myVerticalAlignment = VerticalAlignment(MyVerticalAlignment.self)
-}
-
-extension HorizontalAlignment {
-    private enum MyHorizontalAlignment: AlignmentID {
-        static func defaultValue(in d: ViewDimensions) -> CGFloat {
-            return d[HorizontalAlignment.leading]
-        }
-    }
-    
-    static let myHorizontalAlignment = HorizontalAlignment(MyHorizontalAlignment.self)
-}
-
-extension Alignment {
-    static let myAlignment = Alignment(horizontal: .myHorizontalAlignment, vertical: .myVerticalAlignment)
-}
-
 struct RangeSlider: View {
-    @State var value: ClosedRange<Double> = 0...1
-    
     let lineWidth: CGFloat = 4.0
     let accentColor: Color = Color.Background.selectedInput
     let secondaryColor: Color = Color.Background.white
@@ -48,24 +20,15 @@ struct RangeSlider: View {
                 .fill(Color.Background.defaultInput)
                 .frame(maxHeight: lineWidth)
                 .cornerRadius(2)
-            ZStack(alignment: .myAlignment) {
-                Rectangle()
-                    .fill(accentColor)
-                    .frame(maxWidth: 150, maxHeight: lineWidth)
-                    .cornerRadius(2)
-                SliderHandle(
-                    accentColor: accentColor,
-                    secondaryColor: secondaryColor,
-                    circleRadius: circleRadius
-                ).alignmentGuide(.myHorizontalAlignment) { d in d[.center] }
-                SliderHandle(
-                    accentColor: secondaryColor,
-                    secondaryColor: accentColor,
-                    circleRadius: circleRadius
-                ).alignmentGuide(.myHorizontalAlignment) { d in d[.trailing] }
-            }.alignmentGuide(VerticalAlignment.center, computeValue: { _ in
-                self.circleRadius/2
-            })
+            Rectangle()
+                .fill(accentColor)
+                .frame(maxWidth: 150, maxHeight: lineWidth)
+                .cornerRadius(2)
+            SliderHandle(
+                accentColor: accentColor,
+                secondaryColor: secondaryColor,
+                circleRadius: circleRadius
+            )
         }.padding()
     }
     
@@ -75,26 +38,20 @@ struct RangeSlider: View {
         let circleRadius: CGFloat
         
         var body: some View {
-            VStack {
-                Circle()
-                    .fill(accentColor)
-                    .frame(width: circleRadius, height: circleRadius)
-                    .overlay(
-                        Circle()
-                            .fill(secondaryColor)
-                            .frame(width: circleRadius - circleRadius/2, height: circleRadius - circleRadius/2)
-                )
-                Text("78")
-            }.alignmentGuide(VerticalAlignment.center, computeValue: { _ in
-                self.circleRadius/2
-            })
+            Circle()
+                .fill(accentColor)
+                .frame(width: circleRadius, height: circleRadius)
+                .overlay(
+                    Circle()
+                        .fill(secondaryColor)
+                        .frame(width: circleRadius - circleRadius/2, height: circleRadius - circleRadius/2)
+            )
         }
     }
 }
 
 #if DEBUG
 struct RangeSlider_Previews: PreviewProvider {
-    
     static var previews: some View {
         RangeSlider()
     }
