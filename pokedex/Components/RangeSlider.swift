e3ffc67a8d96b1640018d95f3d9fecc3350c7a38
Victor Yves Crispim <vsilva@corp.globo.com>
2020-05-26 19:49:08 -0300

    organizando os nomes

--- pokedex/Components/RangeSlider.swift
+++ pokedex/Components/RangeSlider.swift
@@ -15,22 +15,24 @@ struct RangeSlider: View {
     let secondaryColor: Color = Color.Background.white
     let thumbDiameter: CGFloat = 20
     let defaultPadding: CGFloat = 8
-    
+    let minimumSpaceBetweenThumbs: CGFloat
    
     @State var leftSpacerLength = CGFloat.zero
     @State var rightSpacerLength = CGFloat.zero
     
-    @Binding var leftValue: Int
-    @Binding var rightValue: Int
+    @Binding var lowerBound: Int
+    @Binding var upperBound: Int
     
     let bounds: ClosedRange<Int>
     let step: Int
     
     public init(selectedLowerBound: Binding<Int>, selectedUpperBound: Binding<Int>, in bounds: ClosedRange<Int>, step: Int = 1) {
-        self._leftValue = selectedLowerBound
-        self._rightValue = selectedUpperBound
+        self._lowerBound = selectedLowerBound
+        self._upperBound = selectedUpperBound
         self.bounds = bounds
         self.step = step
+        
+        minimumSpaceBetweenThumbs = self.thumbDiameter + self.defaultPadding * 2
     }
     
     var body: some View {
@@ -54,29 +56,27 @@ struct RangeSlider: View {
                         )
                         .onChanged { gesture in
                             print("Translado: \(gesture.translation.width)")
-                            let displacement = gesture.translation.width
+                            let xTranslation = gesture.translation.width
                             let trackWidth = geometry.size.width - self.defaultPadding - self.thumbDiameter/2
+                            let newLenght = self.leftSpacerLength + xTranslation
+                            let maxLenght = (geometry.size.width - self.rightSpacerLength) - (self.minimumSpaceBetweenThumbs)
                             
-                            if displacement.isLess(than: 0) {
-                                self.leftSpacerLength = max(self.leftSpacerLength + displacement, 0)
-                                print("Diminuiu para: \(self.leftSpacerLength)")
-                            } else {
-                                self.leftSpacerLength = min(
-                                    self.leftSpacerLength + displacement,
-                                    (geometry.size.width - self.rightSpacerLength) - (self.thumbDiameter + 60)
-                                )
-                                print("Cresceu para: \(self.leftSpacerLength)")
+                            //If dragged to the left
+                            if xTranslation.isLess(than: 0) {
+                                self.leftSpacerLength = max(0, newLenght)
+                            } else { //If dragged to the right
+                                self.leftSpacerLength = min(newLenght, maxLenght)
                             }
 
-                            let leftThumbMiddle = max(0, self.leftSpacerLength)// + self.thumbDiameter/2)
+                            let leftThumbMiddle = max(0, self.leftSpacerLength + self.thumbDiameter/2 + self.defaultPadding)
                             let percentage = Float(leftThumbMiddle/geometry.size.width)
-                            self.leftValue = max(self.bounds.lowerBound, Int(roundf(Float(self.bounds.upperBound) * percentage)))
+                            self.lowerBound = max(self.bounds.lowerBound, Int(Float(self.bounds.upperBound) * percentage))
                             
                             print("\ntrackSize: \(trackWidth)")
                             print("width: \(geometry.size.width)")
                             print("middle: \(leftThumbMiddle)")
                             print("percentage: \(percentage)")
-                            print("leftValue: \(self.leftValue)")
+                            print("leftValue: \(self.lowerBound)")
                         }
                     )
                     .zIndex(1)
@@ -152,11 +152,11 @@ struct RangeSlider: View {
 #if DEBUG
 struct RangeSlider_Previews: PreviewProvider {
     
-    @State static var leftValue = 1
-    @State static var rightValue = 890
+    @State static var lowerBound = 1
+    @State static var upperBound = 890
     
     static var previews: some View {
-        RangeSlider(selectedLowerBound: $leftValue, selectedUpperBound: $rightValue, in: leftValue...rightValue)
+        RangeSlider(selectedLowerBound: $lowerBound, selectedUpperBound: $upperBound, in: lowerBound...upperBound)
             .previewLayout(.fixed(width: 500, height: 300))
     }
 }
