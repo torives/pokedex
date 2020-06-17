ed1d85a9efe861013db2e28c7dca59c0ce6dcbd2
Victor Yves Crispim <vsilva@corp.globo.com>
2020-06-17 20:31:55 -0300

    feat: spaces views following the design guide

--- pokedex/FilterView.swift
+++ pokedex/FilterView.swift
@@ -13,70 +13,83 @@ struct FilterView: View {
     @State private var number = 0.0
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Group {
-                Text("Filters")
-                    .pokemonNameTextStyle()
-                    .padding(.bottom, 5)
-                
-                Text("Use advanced search to explore Pokémon by type, weakness, height and more")
-                    .descriptionTextStyle()
-                    .padding(.bottom, 34)
-            }
-            
-            Group {
-                Text("Types")
-                    .filterTitleTextStyle()
-                
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack(spacing: 20.0) {
-                        ForEach(PokemonType.allCases, id: \.rawValue) { type in
-                            PokemonTypeToggle(type: type, isOn: self.$isOn)
+        ScrollView(.vertical) {
+            VStack(spacing: 50) {
+                VStack(alignment: .leading, spacing: 35) {
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("Filters")
+                            .pokemonNameTextStyle()
+                        
+                        Text("Use advanced search to explore Pokémon by type, weakness, height and more")
+                            .lineLimit(2)
+                            .descriptionTextStyle()
+                            .foregroundColor(Color.Text.grey)
+                    }
+                    
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("Types")
+                            .filterTitleTextStyle()
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack(spacing: 20.0) {
+                                ForEach(PokemonType.allCases, id: \.rawValue) { type in
+                                    PokemonTypeToggle(type: type, isOn: self.$isOn)
+                                }
+                            }
                         }
                     }
-                }
-                
-                Text("Weaknesses")
-                    .filterTitleTextStyle()
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack(spacing: 20.0) {
-                        ForEach(PokemonType.allCases, id: \.rawValue) { type in
-                            PokemonTypeToggle(type: type, isOn: self.$isOn)
+                    
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("Weaknesses")
+                            .filterTitleTextStyle()
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack(spacing: 20.0) {
+                                ForEach(PokemonType.allCases, id: \.rawValue) { type in
+                                    PokemonTypeToggle(type: type, isOn: self.$isOn)
+                                }
+                            }
                         }
                     }
-                }
-                
-                Text("Heights")
-                    .filterTitleTextStyle()
-                HStack(spacing: 20.0) {
-                    ForEach(PokemonHeight.allCases, id: \.rawValue) { height in
-                        PokemonHeightToggle(height: height, isOn: self.$isOn)
+                    
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("Heights")
+                            .filterTitleTextStyle()
+                        HStack(spacing: 20.0) {
+                            ForEach(PokemonHeight.allCases, id: \.rawValue) { height in
+                                PokemonHeightToggle(height: height, isOn: self.$isOn)
+                            }
+                        }
+                    }
+                    
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("Weights")
+                            .filterTitleTextStyle()
+                        HStack(spacing: 20.0) {
+                            ForEach(PokemonWeight.allCases, id: \.rawValue) { weight in
+                                PokemonWeightToggle(weight: weight, isOn: self.$isOn)
+                            }
+                        }
+                    }
+                    
+                    VStack(alignment: .leading, spacing: 20) {
+                        Text("Number Range")
+                            .filterTitleTextStyle()
+                        Slider(value: $number)
                     }
                 }
                 
-                Text("Weights")
-                    .filterTitleTextStyle()
-                HStack(spacing: 20.0) {
-                    ForEach(PokemonWeight.allCases, id: \.rawValue) { weight in
-                        PokemonWeightToggle(weight: weight, isOn: self.$isOn)
-                    }
+                HStack(alignment: .center, spacing: 14) {
+                    Button("Reset") {}
+                        .buttonStyle(SecondaryButtonStyle())
+                        .frame(width: 160, height: 60)
+                    Button("Apply") {}
+                        .buttonStyle(PrimaryButtonStyle())
+                        .frame(width: 160, height: 60)
                 }
             }
-            
-            Text("Number Range")
-                .filterTitleTextStyle()
-            Slider(value: $number)
-            
-            HStack(alignment: .center) {
-                Button("Reset") {}
-                    .buttonStyle(SecondaryButtonStyle())
-                    .frame(width: 160, height: 60)
-                Button("Apply") {}
-                    .buttonStyle(PrimaryButtonStyle())
-                    .frame(width: 160, height: 60)
-            }
-            
-        }.padding()
+            .padding(.horizontal, 40.0)
+            .padding(.top, 30)
+            .padding(.bottom, 50)
+        }
     }
 }
 
