802ac330ef89939572ed454a95adf95e2d56cb78
Victor Yves Crispim <vsilva@corp.globo.com>
2020-06-19 14:51:40 -0300

    feat: create GenerationFilterView

---
+++ pokedex/GenerationFilterView.swift
@@ -0,0 +1,21 @@
+//
+//  GenerationFilterView.swift
+//  pokedex
+//
+//  Created by yves on 19/06/20.
+//  Copyright © 2020 yves. All rights reserved.
+//
+
+import SwiftUI
+
+struct GenerationFilterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GenerationFilterView_Previews: PreviewProvider {
+    static var previews: some View {
+        GenerationFilterView()
+    }
+}
