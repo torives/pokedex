c3cbc38847182c0c5fb0f984d88202efe24282a0
Victor Yves Crispim <vsilva@corp.globo.com>
2020-06-19 15:57:44 -0300

    feat: add generation toggles

--- pokedex/GenerationFilterView.swift
+++ pokedex/GenerationFilterView.swift
@@ -9,8 +9,28 @@
 import SwiftUI
 
 struct GenerationFilterView: View {
+    @State private var isOn = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                ViewHeader(title: "Generations", subtitle: "Use search for generations to explore your Pokémon!")
+                HStack {
+                    VStack {
+                        PokemonGenerationToggle(generation: .I, isOn: self.$isOn)
+                        PokemonGenerationToggle(generation: .III, isOn: self.$isOn)
+                        PokemonGenerationToggle(generation: .V, isOn: self.$isOn)
+                        PokemonGenerationToggle(generation: .VII, isOn: self.$isOn)
+                    }
+                    VStack {
+                        PokemonGenerationToggle(generation: .II, isOn: self.$isOn)
+                        PokemonGenerationToggle(generation: .IV, isOn: self.$isOn)
+                        PokemonGenerationToggle(generation: .VI, isOn: self.$isOn)
+                        PokemonGenerationToggle(generation: .VIII, isOn: self.$isOn)
+                    }
+                }
+            }
+        }
     }
 }
 
