37d2fa896e7861955a21d322d8527ee4aa2c2477
Victor Yves Crispim <vsilva@corp.globo.com>
2020-06-21 21:50:00 -0300

    feat: creates SortView

---
+++ pokedex/SortView.swift
@@ -0,0 +1,21 @@
+//
+//  SortView.swift
+//  pokedex
+//
+//  Created by yves on 21/06/20.
+//  Copyright © 2020 yves. All rights reserved.
+//
+
+import SwiftUI
+
+struct SortView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SortView_Previews: PreviewProvider {
+    static var previews: some View {
+        SortView()
+    }
+}
