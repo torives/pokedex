2f9a8e2c61000bee5f9ab38b06ba8d7f7d3cda65
Victor Yves Crispim <vsilva@corp.globo.com>
2020-06-21 22:03:32 -0300

    feat: creates view contents

--- pokedex/SortView.swift
+++ pokedex/SortView.swift
@@ -10,9 +10,43 @@ import SwiftUI
 
 struct SortView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 35) {
+            ViewHeader(
+                title: "Sort",
+                subtitle: "Sort Pokémons alphabetically or by National Pokédex number!"
+            )
+            
+            VStack(spacing: buttonSpacing) {
+                Button(
+                    action: {},
+                    label: { Text("Smallest number first") }
+                ).buttonStyle(PrimaryButtonStyle())
+                
+                Button(
+                    action: {},
+                    label: { Text("Highest number first") }
+                ).buttonStyle(PrimaryButtonStyle())
+                
+                Button(
+                    action: {},
+                    label: { Text("A-Z") }
+                ).buttonStyle(PrimaryButtonStyle())
+                
+                Button(
+                    action: {},
+                    label: { Text("Z-A") }
+                ).buttonStyle(PrimaryButtonStyle())
+            }
+            .frame(maxHeight: 3 * buttonSpacing + 4 * buttonHeight)
+        }
+        .padding(.top, 30)
+        .padding(.bottom, 50)
+        .padding(.horizontal, 40)
     }
 }
+
+private let buttonSpacing: CGFloat = 20
+private let buttonHeight: CGFloat = 60
 
 struct SortView_Previews: PreviewProvider {
     static var previews: some View {
