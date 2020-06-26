b284dbf8b4f6aa0fa860a1fbc0098be6a7cd8d4e
Victor Yves Crispim <vsilva@corp.globo.com>
2020-06-25 21:12:49 -0300

    feat: creates view layout

--- pokedex/Components/Custom/PokemonEvolutionView.swift
+++ pokedex/Components/Custom/PokemonEvolutionView.swift
@@ -9,13 +9,37 @@
 import SwiftUI
 
 struct PokemonEvolutionView: View {
+    let evolutionLevel: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 30) {
+            PokemonEvolutionStageView(
+                pokemonName: "Bulbassaur",
+                pokemonNumber: "001",
+                pokemonImage: Image("gen-I-grass-starter")
+            )
+            
+            VStack(spacing: 5) {
+                Image(systemName: "arrow.right")
+                    .resizable()
+                    .frame(width: 25, height: 25)
+                    .opacity(0.1)
+                Text("(\(self.evolutionLevel))")
+                    .pokemonNumberTextStyle()
+            }
+            .padding(.horizontal)
+            
+            PokemonEvolutionStageView(
+                pokemonName: "Bulbassaur",
+                pokemonNumber: "001",
+                pokemonImage: Image("gen-I-grass-starter")
+            )
+        }
     }
 }
 
 struct PokemonEvolutionView_Previews: PreviewProvider {
     static var previews: some View {
-        PokemonEvolutionView()
+        PokemonEvolutionView(evolutionLevel: "Level 16")
     }
 }
