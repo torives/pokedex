36d9b3da329dd8bf47b09d54e45cdc02b803b523
Victor Yves Crispim <vsilva@corp.globo.com>
2020-06-25 22:23:01 -0300

    feat: creates view layout

--- pokedex/Components/Custom/PokemonListItem.swift
+++ pokedex/Components/Custom/PokemonListItem.swift
@@ -10,7 +10,51 @@ import SwiftUI
 
 struct PokemonListItem: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Group {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color.Pokemon.grassBackground)
+                    .frame(height: 115)
+                
+                Color.Gradients.dotPattern
+                    .mask(Image("6x3").resizable().scaledToFit())
+                    .frame(width: 74, height: 32)
+                    .offset(x: -40, y: -35)
+                
+                Color.Gradients.dotPattern
+                    .mask(Image("pokeball").resizable().scaledToFit())
+                    .frame(width: 145, height: 145)
+                    .offset(x: 110, y: 0)
+                
+                HStack {
+                    VStack(alignment: .leading, spacing: 5) {
+                        VStack(alignment: .leading, spacing: 0) {
+                            Text("#001")
+                                .pokemonNumberTextStyle()
+                            Text(verbatim: "Bulbasaur")
+                                .foregroundColor(Color.Text.white)
+                                .pokemonNameTextStyle()
+                        }
+                        HStack(spacing: 5) {
+                            Badge(type: .grass, showLabel: true)
+                            Badge(type: .poison, showLabel: true)
+                        }
+                    }
+                    
+                    Spacer()
+                    
+                    Image("gen-I-grass-starter")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 130, height: 130)
+                        .offset(x: 0, y: -20)
+                }
+                .padding(.leading, 20)
+                .padding(.trailing, 10)
+            }
+            .offset(x: 0, y: 12)
+        }
+        .frame(width: 334, height: 140)
     }
 }
 
