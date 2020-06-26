43b9693cebbadf2f2701e32a8c526bdb24e6913c
Victor Yves Crispim <vsilva@corp.globo.com>
2020-06-25 21:14:27 -0300

    feat: creates PokemonListItem

---
+++ pokedex/Components/Custom/PokemonListItem.swift
@@ -0,0 +1,21 @@
+//
+//  PokemonListItem.swift
+//  pokedex
+//
+//  Created by yves on 25/06/20.
+//  Copyright © 2020 yves. All rights reserved.
+//
+
+import SwiftUI
+
+struct PokemonListItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PokemonListItem_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonListItem()
+    }
+}
